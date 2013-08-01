0ccf5878ec311a731bfc458f03f22623e474a301
Hugh Halford-Thompson <Hughht5@gmail.com>
2013-08-01 14:33:13 +0100

    test

--- app.js
+++ app.js
@@ -33,10 +33,6 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-app.get('/hi', function (req, res) {
-  res.end('Hi there hugh!')
-})
-
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
