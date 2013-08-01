48b0ba1cd890fb433b82f461e4fa2061a0d91299
Hugh Halford-Thompson <Hughht5@gmail.com>
2013-08-01 21:19:35 +0100

    adding rest for mongo

--- public/js/getCover.js
+++ public/js/getCover.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("#submit").submit(function() {
     	console.log("submitting new class");
         //Do the AJAX post
-        $.post($("#getCoverForm").attr("action"), $("#html-form").serialize(), function(data){
+        $.post($("#getCoverForm").attr("action"), $("#getCoverForm").serialize(), function(data){
             //do stuff here...
         });
         //Stop the normal POST
