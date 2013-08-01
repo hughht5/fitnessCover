b5a2a0660bedb5bb866dcdc3ad86faf806837992
Hugh Halford-Thompson <Hughht5@gmail.com>
2013-08-01 13:42:42 +0100

    test

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ if ('development' == app.get('env')) {
 }
 
 //app.get('/', routes.index);
-app.get('/', 'public/index.html');
+app.get('/index', 'public/index.html');
 app.get('/users', user.list);
 app.get()
 
