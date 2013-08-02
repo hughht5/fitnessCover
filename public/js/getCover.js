0ea76c4f38d1fd5144405733a48212389031d469
Hugh Halford-Thompson <Hughht5@gmail.com>
2013-08-02 12:49:27 +0100

    saving cover requests now works

--- public/js/getCover.js
+++ public/js/getCover.js
@@ -1,25 +1,12 @@
 function submitCover(){
 	console.log("submitting new class");
+    
     //Do the AJAX post
     $.post("/api/classes", $("#getCoverForm").serialize(), function(data){
-        //do stuff here...
+        //notify user the request has been saved
+        alert("Your request for cover has been saved. You will be notified by email to confirm whether we find cover for you.")
     });
+
     //Stop the normal POST
     return false;
 }
-/*
-$(document).ready(function(){
-    document.getElementById('submit').onsubmit = function(e) {
-
-    	e.preventDefault
-
-    	console.log("submitting new class");
-        //Do the AJAX post
-        $.post("/api/classes", $("#getCoverForm").serialize(), function(data){
-            //do stuff here...
-        });
-        //Stop the normal POST
-        return false;
-    };
-});
-//*/
