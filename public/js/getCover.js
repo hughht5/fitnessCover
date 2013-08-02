cfa7ffa86d8d7588ca823922c0be33dc7dbfa371
Hugh Halford-Thompson <Hughht5@gmail.com>
2013-08-02 15:29:03 +0100

    bootbox alerts added

--- public/js/getCover.js
+++ public/js/getCover.js
@@ -4,7 +4,9 @@ function submitCover(){
     //Do the AJAX post
     $.post("/api/classes", $("#getCoverForm").serialize(), function(data){
         //notify user the request has been saved
-        alert("Your request for cover has been saved. You will be notified by email to confirm whether we find cover for you.")
+    	bootbox.alert("Your request for cover has been saved. You will be notified by email to confirm whether we find cover for you.");
+    	
+    	//reset the form
     	document.getElementById("getCoverForm").reset();
     });
 
