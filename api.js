8ce7b6662f5ec982ea83a9e1c28cccc2ab60e066
Hugh Halford-Thompson <Hughht5@gmail.com>
2013-08-08 14:49:41 +0100

    testing update to approve instriuctors

--- api.js
+++ api.js
@@ -173,7 +173,7 @@ exports.approveInstructor = function(req, res) {
 
     instructordb.collection('instructors', function(err, collection) {
         //collection.update(v1,v2).toArray(function(err, items) {
-        collection.update({'_id':new BSON.ObjectID(id)}, instructor, {safe:true}, function(err, result) {
+        collection.update({'_id':new BSON.ObjectID(id)}, '$set:{confirmed:true}', {safe:true}, function(err, result) {
             res.send(result);
         });
     });
