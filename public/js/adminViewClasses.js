64b35546b89fca62f3607571340cd16305d6c085
Hugh Halford-Thompson <Hughht5@gmail.com>
2013-08-11 14:57:33 +0100

    admin view classes

---
+++ public/js/adminViewClasses.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+
+    getClasses();
+
+});
+
+
+//query mongo and add results to the table
+function getClasses(){
+    $.get("/api/classes", function(data){
+        for (var x=0;x<data.length;x++){
+            addRow(data[x]);
+        }
+    });
+}
+
+function addRow(coverClass){
+
+    var table=document.getElementById("classes");
+    var row=table.insertRow(-1);
+
+    var firstName=row.insertCell(0);
+    var lastName=row.insertCell(1);
+    var email=row.insertCell(2);
+    var mobile=row.insertCell(3);
+    var gymName=row.insertCell(4);
+    var classType=row.insertCell(5);
+    var classDate=row.insertCell(6);
+    var classTime=row.insertCell(7);
+    var classRate = row.insertCell(8);
+    var reason = row.insertCell(9);
+    var notes = row.insertCell(10);
+    var instructorAssigned = row.insertCell(11);
+    var instructorPaid = row.insertCell(12);
+    var gymInvoiced = row.insertCell(13);
+    var paidByGym = row.insertCell(14);
+    var remove = row.insertCell(15);
+
+
+    firstName.innerHTML=coverClass.firstName;
+    lastName.innerHTML=coverClass.lastName;
+    email.innerHTML=coverClass.email;
+    mobile.innerHTML=coverClass.mobile;
+    gymName.innerHTML=coverClass.gymName;
+    classType.innerHTML=coverClass.classType;
+    classDate.innerHTML=coverClass.classDate;
+    classTime.innerHTML=coverClass.classTime;
+    classRate.innerHTML=coverClass.classRate;
+    reason.innerHTML=coverClass.reason;
+    notes.innerHTML=coverClass.notes;
+    instructorAssigned.innerHTML=coverClass.instructorAssigned;
+    instructorPaid.innerHTML=coverClass.instructorPaid;
+    gymInvoiced.innerHTML=coverClass.gymInvoiced;
+    paidByGym.innerHTML=coverClass.paidByGym;
+
+    var buttonHTML='<button onclick="removeClass(\''+coverClass._id+'\')">Remove</button>';
+    remove.innerHTML = buttonHTML;
+
+}
+
+
+function removeClass(id){
+
+    $.ajax({
+        url: "/api/classes/"+id,
+        type: 'DELETE',
+        success: function(data) {
+            console.log(data);
+        }
+    });
+
+
+    //now reload the table
+    //remove all rows except title
+    $("#classes>thead>tr:not(:first)").remove();
+
+    //reload
+    getClasses();
+}
+
