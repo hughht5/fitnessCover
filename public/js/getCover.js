33b54da4208414aed217b36c206238358e387a24
Hugh Halford-Thompson <Hughht5@gmail.com>
2013-09-26 11:02:04 +0100

    getCover message update with optional redirect

--- public/js/getCover.js
+++ public/js/getCover.js
@@ -62,11 +62,28 @@ function submitCover(){
     //Do the AJAX post
     $.post("/api/classes", $("#getCoverForm").serialize(), function(data){
         
-        //notify user the request has been saved
-    	bootbox.alert("Thank you, your request for cover has been saved. We will email you to confirm.");
-    	
-    	//reset the form
-    	document.getElementById("getCoverForm").reset();
+        bootbox.dialog({
+          title: "Thank you for submitting a cover request!",
+          message: "Your request has been saved and you will receive an email confirming it momentarily. What would you like to do now?",
+          buttons: {
+            success: {
+              label: "Go to home page.",
+              className: "btn-success",
+              callback: function() {
+                //redirect to home
+                window.location.href = "../";
+              }
+            },
+            main: {
+              label: "Add another class.",
+              className: "btn-primary",
+              callback: function() {
+                //reset the form
+                document.getElementById("getCoverForm").reset();
+              }
+            }
+          }
+        });
 
     });
 
